c19c1fc3150a5fd123a1def33050f938787d4447
Sylvie <yuanqi1368@example.com>
2024-08-21 10:46:45 +0800

    refactor: import PropTypes

--- wordle-practice/src/App.jsx
+++ wordle-practice/src/App.jsx
@@ -1,26 +1,28 @@
 import "./App.css";
-import React, { useEffect, useReducer } from "react";
+import PropTypes from "prop-types";
+import { useEffect, useReducer } from "react";
 import { WordReducer } from "./WordReducer";
 import { letterStatus, colorStatus } from "./constants/constant";
 
 function App() {
   const [state, dispatch] = useReducer(WordReducer, initialState);
-  const handleKeyDown = (e) => {
-    const { key } = e;
-    if (key.length === 1 && /^[A-Z]$/i.test(key) && state.gameStatus === 0) {
-      dispatch({ type: "INPUT_LETTER", letter: key.toUpperCase() });
-    } else if (key === "Backspace") {
-      dispatch({ type: "DELETE_LETTER" });
-    } else if (key === "Enter") {
-      dispatch({ type: "SUBMIT_GUESS" });
-    }
-  };
+
   useEffect(() => {
+    const handleKeyDown = (e) => {
+      const { key } = e;
+      if (key.length === 1 && /^[A-Z]$/i.test(key) && state.gameStatus === 0) {
+        dispatch({ type: "INPUT_LETTER", letter: key.toUpperCase() });
+      } else if (key === "Backspace") {
+        dispatch({ type: "DELETE_LETTER" });
+      } else if (key === "Enter") {
+        dispatch({ type: "SUBMIT_GUESS" });
+      }
+    };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  }, [state.gameStatus]);
 
   return (
     <>
@@ -40,16 +42,24 @@ const Grid = ({ grid, statusGrid }) => {
     </div>
   );
 };
+Grid.propTypes = {
+  grid: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
+  statusGrid: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
+};
 
 const Cell = ({ value, status }) => <div className={`cell ${colorStatus[status]}`}>{value}</div>;
+Cell.propTypes = {
+  value: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+};
 
 const initialState = {
   grid: Array(6)
     .fill()
-    .map(() => Array(5).fill("")), 
+    .map(() => Array(5).fill("")),
   statusGrid: Array(6)
     .fill()
-    .map(() => Array(5).fill(letterStatus.idle)), 
+    .map(() => Array(5).fill(letterStatus.idle)),
   currentRow: 0,
   currentCol: 0,
   answer: "APPLE",
